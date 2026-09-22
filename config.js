0773ada474471938797cb2ea959fde23c5dbc6e6
Ian2003227 <mynameisian2003227@gmail.com>
2026-09-22 13:53:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // 婚禮報到系統設定
+// 婚禮報到系統設定
 const CONFIG = {
-  API_URL: "PLACEHOLDER_API_URL",
-  API_KEY: "PLACEHOLDER_API_KEY"
+  API_URL: "https://script.google.com/macros/s/AKfycbz5GDVa7nEp4zhqWmgclQMAqjHOWT8kuWZcUZGNSfyfL9Dc-sNKs41UyofQGcSdm1p-/exec",
+  API_KEY: "ffbc306971444a9e824a2bb6e0324f45"
 };
